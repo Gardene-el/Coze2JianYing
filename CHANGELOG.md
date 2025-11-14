2caf3cda12ba77d0268e83b484a78f80c1c20f39
semantic-release <semantic-release>
2025-11-14 20:46:30 +0000

    chore(release): 1.0.0 [skip ci]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,3 +9,7 @@
 ## v1.0.0 (2025-11-14)
 
 - Initial Release
+
+## v1.0.0 (2025-11-14)
+
+- Initial Release
--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ Coze2JianYing Draft Generator Application
 草稿生成器应用
 """
 
-__version__ = "0.0.4"
+__version__ = "1.0.0"
 __app_name__ = "CozeJianYingDraftGenerator"
 __author__ = "Gardene-el"
 __github__ = "https://github.com/Gardene-el"
