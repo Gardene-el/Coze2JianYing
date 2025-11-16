0761a8c570e9d391c91133899692254bdd451198
Garden_eel <103943173+Gardene-el@users.noreply.github.com>
2025-11-16 12:57:32 +0800

    Docs: Delete coze_plugin/tests directory

--- coze_plugin/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# Coze2JianYing Test Suite
-"""
-Test suite for the Coze2JianYing project.
-
-This directory contains all test files for the project, organized to validate
-different aspects of the Coze-to-JianYing automation workflow.
-"""
